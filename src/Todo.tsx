4dac633f0384892bb3fffdef67b4fa06d99acadd
chucker34 <kawakami34@gmail.com>
2018-06-17 17:21:11 +0900

    雑なボタンtoggle

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -3,19 +3,37 @@ import * as React from 'react';
 import { Text, View, Button } from 'native-base';
 import ITodo from './interfaces/ITodo';
 
-export default class Todo extends React.Component<ITodo, {}> {
+interface State {
+  status: boolean
+}
+
+export default class Todo extends React.Component<ITodo, State> {
+
+  constructor(props: ITodo) {
+    super(props)
+    this.state = {
+      status: this.props.status
+    }
+  }
+
   render() {
-    const link = this.props.status ? '元に戻す' : '完了！'
+    const link = this.state.status ? '元に戻す' : '完了！'
 
     return(
       <View>
         <Text>{this.props.id}：{this.props.title}</Text>
-        <Button>
+        <Button block onPress={() => { this.showAlert(); }}>
           <Text>{link}</Text>
         </Button>
         <Text>{this.props.desc}</Text>
       </View>
     );
+  }
+
+  showAlert() {
+    this.setState(() => {
+      return { status: !this.state.status }
+    })
   }
 }
 
