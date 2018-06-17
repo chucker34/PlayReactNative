73a9eb69867f45f6e660cd7e7a6edc1dd6b9b770
chucker34 <kawakami34@gmail.com>
2018-06-17 17:07:18 +0900

    install native-base

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,18 +1,18 @@
 
 import * as React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button } from 'native-base';
 import ITodo from './interfaces/ITodo';
 
 export default class Todo extends React.Component<ITodo, {}> {
   render() {
     const link = this.props.status ? '元に戻す' : '完了！'
+
     return(
       <View>
         <Text>{this.props.id}：{this.props.title}</Text>
-        <Button
-          onPress={() => { console.log('ボタン押したよ')}}
-          title={link}
-        />
+        <Button>
+          <Text>{link}</Text>
+        </Button>
         <Text>{this.props.desc}</Text>
       </View>
     );
