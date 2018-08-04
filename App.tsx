a27df0d0c6dde170838628edffa0ff3289b52ac1
chucker34 <kawakami34@gmail.com>
2018-08-04 22:43:25 +0900

    雑に画面遷移

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-import { View, Container } from 'native-base'
+import { View, Container, Button, Text } from 'native-base'
+import { Actions, ActionConst, Scene, Router } from 'react-native-router-flux'
 import TodoList from './src/TodoList';
 import ITodo from './src/interfaces/ITodo';
 
@@ -23,7 +24,22 @@ const defaultProps: ITodo[] = [
   },
 ]
 
-export default class App extends React.Component<ITodo[], State> {
+class Dummy extends React.Component {
+  render() {
+    return (
+      <Container>
+        <View>
+          <Text>Dummy</Text>
+          <Button onPress={Actions.main}>
+            <Text>次のページ</Text>
+          </Button>
+        </View>
+      </Container>
+    )
+  }
+}
+
+class Main extends React.Component<ITodo[], State> {
   constructor(props: ITodo[]) {
     super(props);
     this.state = {
@@ -34,10 +50,29 @@ export default class App extends React.Component<ITodo[], State> {
   render() {
     return (
       <Container>
-        <View style={styles.container}>
+       <View style={styles.container}>
+          <View>
+            <Button onPress={() => { Actions.reset('dummy')}}>
+              <Text>戻る</Text>
+            </Button>
+          </View>
           <TodoList todos={this.state.todos} />
         </View>
       </Container>
+    )
+  }
+}
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+     <Router>
+        <Scene key="root">
+          <Scene key="dummy" initial component={Dummy} title="Dummy" />
+          <Scene key="main" component={Main} title="Main" />
+        </Scene>
+      </Router>
     );
   }
 }
